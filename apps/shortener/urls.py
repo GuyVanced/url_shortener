7a31102b31285908f28746059fc74a71b772a7eb
GuyVanced <pandeykushal017@gmail.com>
2026-02-03 22:12:45 +0545

    chore : Project Setup and boilerplate stuffs

---
+++ apps/shortener/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = "shortener"
+
+urlpatters = [
+    path("", views.dashboard, name= "dashboard"),
+    path("<str:short_code>", views.redirect_view, name = "redirect")
+]
---
+++ apps/shortener/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def dashbaord(request):
+    return HttpResponse("Hello Dashboard")
+
+def redirect_view(request, short_code):
+    return HttpResponse(f"Redirecting to users {short_code}")
+
