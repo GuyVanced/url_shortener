f1f66a142c3bed47c70d9ec2d7b3f438e2816d8d
GuyVanced <pandeykushal017@gmail.com>
2026-02-03 23:28:41 +0545

    feat : making ShortUrl Model manageable in django admin

--- apps/shortener/admin.py
+++ apps/shortener/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import ShortUrl
 
-# Register your models here.
+@admin.register(ShortUrl)
+class ShortURLAdmin(admin.ModelAdmin):
+    list_display = (
+        "short_code",
+        "original_url",
+        "user",
+        "click_count",
+        "created_at",
+        "expires_at",
+        "is_active",
+    )
+    search_fields = ("short_code", "original_url", "user__username")
+    list_filter = ("created_at", "is_active")
