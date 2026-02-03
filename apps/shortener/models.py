a219f1e7944a8d549f1412cdef6e735e48578675
GuyVanced <pandeykushal017@gmail.com>
2026-02-03 22:53:01 +0545

    feat : created the ShortUrl model

--- apps/shortener/models.py
+++ apps/shortener/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ShortUrl(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name = "short_urls"
+    )
+
+    original_url = models.URLField()
+    short_code = models.CharField(max_length=10)
+    click_count = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expired_at = models.DateTimeField(blank=True, null = True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.short_code} -> {self.original_url}"
+
