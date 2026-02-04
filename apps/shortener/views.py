55e41887fdc6a90d1acffe86f94c39d40bb96623
GuyVanced <pandeykushal017@gmail.com>
2026-02-05 01:07:36 +0545

    feat : wrote view for creating short url

--- apps/shortener/views.py
+++ apps/shortener/views.py
@@ -1,9 +1,41 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from .forms import ShortUrlForm
+from .services import generate_unique_short_code
+from .models import ShortUrl
+from django.contrib.auth.decorators import login_required
 
 def dashboard(request):
     return HttpResponse("Hello Dashboard")
 
-def redirect_view(request, short_code):
-    return HttpResponse(f"Redirecting to users shortcode : {short_code}")
+
+
+
+@login_required
+def create_short_url(request):
+    if request.method == "POST":
+        form = ShortUrlForm(request.POST)
+        if form.is_valid():
+            original_url = form.cleaned_data["original_url"]
+
+            short_code = generate_unique_short_code()
+
+            short_url = ShortUrl.objects.create(
+                user = request.user,
+                original_url = original_url,
+                short_code = short_code
+            )
+            return render(request,
+                          "shortener/create_success.html",
+                          {"short_url": short_url})
+    else:
+        form = ShortUrlForm()
+
+    return render(request,
+                  "shortener/create.html", {"form": form})
+
+
+
+
+
 
