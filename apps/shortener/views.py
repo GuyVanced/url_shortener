37883c930dbd4f52f4ffe88b3af9cc7513cf95b5
GuyVanced <pandeykushal017@gmail.com>
2026-02-05 16:31:21 +0545

    feat : added edit short url view

--- apps/shortener/views.py
+++ apps/shortener/views.py
@@ -80,6 +80,20 @@ def delete_short_url(request, pk):
         request,"shortener/confirm_delete.html", {"short_url":short_url}
     )
 
+@login_required
+def edit_url(request,pk):
+    short_url = get_object_or_404(ShortUrl, pk = pk, user= request.user)
+
+    if request.method == "POST":
+        form = ShortUrlEditForm(request.POST, instance = short_url)
+        if form.is_valid():
+            form.save()
+            return redirect("shortener:dashboard")
+    else:
+            form = ShortUrlForm(instance = short_url)
+        
+    return render(request, "shortener/edit_url.html", {"form": form, "short_url": short_url})
+
 
 
 
