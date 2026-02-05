65267bf899ab53101f67cf8355e118468f3bab7e
GuyVanced <pandeykushal017@gmail.com>
2026-02-05 19:52:13 +0545

    chore: config changes

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', signup_view, name = "signup"),
     path('logout/', auth_views.LogoutView.as_view(next_page = "login"), name='logout'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(next_page = ""), name='login'),
     path('', include("apps.shortener.urls")),
 ]
