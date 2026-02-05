09d13dd2089153823cc25202dab175ea35ef2352
GuyVanced <pandeykushal017@gmail.com>
2026-02-05 21:17:00 +0545

    ops: fixed csrf 403 error

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("SECRET_KEY", default="unsafe-secret-key")
 DEBUG = config("DEBUG", default=False, cast=bool)
 
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://urlshortener-production-4598.up.railway.app/",]
 
 
 # Application definition
