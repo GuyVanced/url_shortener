21cc3789db5929dc8be1ca0625b6de4f3327e231
GuyVanced <pandeykushal017@gmail.com>
2026-02-05 19:52:50 +0545

    feat: Setup final routes

--- apps/shortener/urls.py
+++ apps/shortener/urls.py
@@ -4,6 +4,10 @@ from . import views
 app_name = "shortener"
 
 urlpatterns = [
-    path("", views.dashboard, name= "dashboard"),
-    path("<str:short_code>", views.redirect_view, name = "redirect")
+    path("dashboard/", views.dashboard, name= "dashboard"),
+    path("create/", views.create_short_url, name= "create_short_url"),
+    path("delete/<int:pk>", views.delete_short_url, name="delete_short_url"),
+    path("urls/<int:pk>/edit/", views.edit_url, name="edit_url"),
+    path("<str:short_code>/", views.redirect_short_url, name = "redirect"),
+    
 ]
