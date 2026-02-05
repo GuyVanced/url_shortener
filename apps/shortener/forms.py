b5fffc815e27212162c492d6c0b0483c3f12af9c
GuyVanced <pandeykushal017@gmail.com>
2026-02-05 16:30:35 +0545

    feat: made a ModelForm for edit view

--- apps/shortener/forms.py
+++ apps/shortener/forms.py
@@ -5,3 +5,8 @@ class ShortUrlForm(forms.ModelForm):
     class Meta:
         model = ShortUrl
         fields = ["original_url"]
+
+class ShortUrlEditForm(forms.ModelForm):
+    class Meta:
+        model = ShortUrl
+        fields = ["original_url", "expires_at", "is_active"]
