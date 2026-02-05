fc982fbf0c3725b32c10697be2b33dad280ad07b
GuyVanced <pandeykushal017@gmail.com>
2026-02-05 23:36:24 +0545

    chore: final UI/UX touchups

--- config/settings.py
+++ config/settings.py
@@ -122,6 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Media files (User uploaded content)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/dashboard/"
 LOGOUT_REDIRECT_URL = "/login/"
